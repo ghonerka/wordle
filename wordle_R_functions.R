6ad5ded7ebff20d07602970010e5267a9ae69766
Gordon Honerkamp-Smith <ghonerka@gmail.com>
2022-04-12 12:32:20 -0700

    Added function wordle2() that creates all possible permutations that are valid given gray, yellow, and green hints

--- wordle_R_functions.R
+++ wordle_R_functions.R
@@ -36,3 +36,59 @@ wordle1 <- function(letters_known, positions_known) {
   apply(dd, MARGIN = 1, FUN = guess_word, letters = letters_known)
 }
 
+
+
+
+
+wordle2 <- function(gray, yellow, green) {
+  require(dplyr)
+  
+  ##### Green Hints
+  
+  # All possible permutations that conform to the green hints
+  
+  all_words <- green |> 
+    lapply(FUN = \(x) if (x == "") LETTERS else x) |> 
+    do.call(what = expand.grid)
+  
+  
+  
+  ##### Gray Hints
+  
+  # Remove words that contain any gray letters
+  no_gray <- all_words |> 
+    filter(!if_any(.fns = ~ .x %in% gray))
+  
+  
+  
+  ##### Yellow Hints
+  
+  # Remove words that have letters coinciding with the yellow letters
+  remove_yellow <- no_gray |> 
+    filter(
+      !(L1 %in% yellow$`1`), 
+      !(L2 %in% yellow$`2`), 
+      !(L3 %in% yellow$`3`), 
+      !(L4 %in% yellow$`4`), 
+      !(L5 %in% yellow$`5`))
+  
+  # Require that words contain all yellow letters
+  unique_yellow <- unlist(yellow) |> 
+    unique() |> 
+    setdiff(y = "")
+  has_yellow <- remove_yellow |> 
+    rowwise() |> 
+    filter(all(unique_yellow %in% c(L1, L2, L3, L4, L5))) |> 
+    ungroup()
+  
+  
+  
+  ##### Print valid words
+  
+  # Require that words agree with green hints
+  valid_words <- has_yellow |> 
+    mutate(word = paste0(L1, L2, L3, L4, L5)) |> 
+    pull(word)
+  
+  return(valid_words)
+}
